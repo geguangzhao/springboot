48bdbe2179ba72f467817a6a80cabc7107c10133
geguangzhao <geguangzhaogis@163.com>
2019-10-17 18:15:32 +0800

    tij

--- src/main/java/com/example/SpringBootStartApplication.java
+++ src/main/java/com/example/SpringBootStartApplication.java
@@ -8,7 +8,6 @@ public class SpringBootStartApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         // 注意这里要指向原先用main方法执行的Application启动类
-    	//
         return builder.sources(DemoApplication.class);
     }
 }
